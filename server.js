a8767e68a29ac746d874b450b0a245eb44210e6a
SanyaREZYA <alex07082005m@gmail.com>
2025-04-12 17:11:58 +0300

    init project

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/js', express.static(path.join(__dirname, 'js')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'index.html'));
+});
+
+app.get('/aboutus', (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'aboutus.html'));
+});
+
+app.get('/advert', (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'advert.html'));
+});
+
+app.get('/company', (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'company.html'));
+});
+
+app.get('/placement', (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'placement.html'));
+});
+
+app.get('/search', (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'search.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Сервер запущено на http://localhost:${PORT}`);
+});
