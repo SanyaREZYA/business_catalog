1188b49f1133a9ee5c051c0e142f804c04215c30
SanyaREZYA <alex07082005m@gmail.com>
2025-04-12 17:22:14 +0300

    add db connect

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
+const pool = require("./db");
 const PORT = process.env.PORT || 3000;
 
 app.use('/css', express.static(path.join(__dirname, 'css')));
