a91a4c70f54088c0b7a04eb773019a8b20e9b9a8
SanyaREZYA <alex07082005m@gmail.com>
2025-04-14 13:45:09 +0300

    add all get request

--- server.js
+++ server.js
@@ -44,10 +44,105 @@ app.get('/companies', async (req, res) => {
 
 app.get('/companies/:id', async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM companies WHERE id = ?', [req.params.id]);
+    const result = await pool.query('SELECT * FROM companies WHERE id = $1', [req.params.id]);
     res.json(result.rows);
   } catch (err) {
     console.error('Error getting companies:', err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/companies/search/:name', async (req, res) => {
+  const { name } = req.params;
+
+  try {
+    const result = await pool.query(
+      `SELECT *
+       FROM companies
+       WHERE LOWER(name) LIKE LOWER($1)`,
+      [`%${name}%`],
+    );
+
+    res.json(result.rows);
+  } catch (err) {
+    console.error('Error searching companies by name:', err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/companies-by-tag-id/:tagId', async (req, res) => {
+  const { tagId } = req.params;
+
+  try {
+    const result = await pool.query(
+      `SELECT c.*
+       FROM companies c
+                JOIN company_tags ct ON c.id = ct.company_id
+       WHERE ct.id = $1`,
+      [tagId],
+    );
+
+    res.json(result.rows);
+  } catch (err) {
+    console.error('Error fetching companies by tag ID:', err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/companies-by-tag-name/:tagName', async (req, res) => {
+  const { tagName } = req.params;
+
+  try {
+    const result = await pool.query(
+      `SELECT DISTINCT c.*
+       FROM companies c
+                JOIN company_tags ct ON c.id = ct.company_id
+       WHERE LOWER(ct.tag) LIKE LOWER($1)`,
+      [`%${tagName}%`],
+    );
+
+    res.json(result.rows);
+  } catch (err) {
+    console.error('Error fetching companies by tag name:', err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+app.get('/companies-by-area-id/:areaId', async (req, res) => {
+  const { areaId } = req.params;
+
+  try {
+    const result = await pool.query(
+      `SELECT c.*
+       FROM companies c
+                JOIN activity_areas aa ON aa.id = c.activity_area_id
+       WHERE aa.id = $1`,
+      [areaId],
+    );
+
+    res.json(result.rows);
+  } catch (err) {
+    console.error('Error fetching companies by area ID:', err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/companies-by-category-id/:categoryId', async (req, res) => {
+  const { categoryId } = req.params;
+
+  try {
+    const result = await pool.query(
+      `SELECT c.*
+       FROM companies c
+                JOIN categories cc ON cc.id = c.category_id
+       WHERE cc.id = $1`,
+      [categoryId],
+    );
+
+    res.json(result.rows);
+  } catch (err) {
+    console.error('Error fetching companies by category ID:', err);
     res.status(500).send('Internal Server Error');
   }
 });
@@ -78,6 +173,16 @@ app.get('/activity-areas', async (req, res) => {
     res.json(result.rows);
   } catch (err) {
     console.error('Error getting activity areas:', err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/company-tags', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM company_tags');
+    res.json(result.rows);
+  } catch (err) {
+    console.error('Error getting company tags:', err);
     res.status(500).send('Internal Server Error');
   }
 });
